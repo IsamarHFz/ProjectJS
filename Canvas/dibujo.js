8a358ddd3db191f82d5985f52def0c08ef954593
IsabelHernandez <marisahfz07@gmail.com>
2023-10-18 13:02:53 -0600

    06 Canvas con HTML y Javascript

---
+++ Canvas/dibujo.js
@@ -0,0 +1,151 @@
+const lienzo = document.querySelector('#lienzo');
+
+const ctx = lienzo .getContext('2d');
+
+// tx.beginPath();
+// ctx.fillStyle = 'black';
+// ctx.arc(200, 200, 200, 0, Math.PI * 2, true);
+// ctx.fill();
+
+ctx.fillStyle = 'Green';
+ctx.fillRect(150, 50, 30, 30); 
+ctx.strokeRect(150, 50, 30, 30); 
+ctx.fillRect(180, 80, 30, 30); 
+ctx.strokeRect(180, 80, 30, 30); 
+ctx.fillRect(180, 50, 30, 30); 
+ctx.strokeRect(180, 50, 30, 30); 
+
+ctx.fillStyle = 'Orange';
+ctx.fillRect(180, 110, 30,30 );
+ctx.strokeRect(180, 110, 30,30 );
+ctx.fillRect(150, 110, 30,30 );
+ctx.strokeRect(150, 110, 30,30 );
+ctx.fillRect(120, 110, 30,30 );
+ctx.strokeRect(120, 110, 30,30 );
+ctx.fillRect(210, 110, 30,30 );
+ctx.strokeRect(210, 110, 30,30 );
+ctx.fillRect(240, 110, 30,30 );
+ctx.strokeRect(240, 110, 30,30 );
+
+ctx.fillRect(180, 140, 30,30 );
+ctx.strokeRect(180, 140, 30,30 );
+ctx.fillRect(150, 140, 30,30 );
+ctx.strokeRect(150, 140, 30,30 );
+ctx.fillRect(120, 140, 30,30 );
+ctx.strokeRect(120, 140, 30,30 );
+ctx.fillRect(90, 140, 30,30 );
+ctx.strokeRect(90, 140, 30,30 );
+ctx.fillRect(210, 140, 30,30 );
+ctx.strokeRect(210, 140, 30,30 );
+ctx.fillRect(240, 140, 30,30 );
+ctx.strokeRect(240, 140, 30,30 );
+ctx.fillRect(270, 140, 30,30 );
+ctx.strokeRect(270, 140, 30,30 );
+
+ctx.fillRect(180, 170, 30,30 );
+ctx.strokeRect(180, 170, 30,30 );
+ctx.fillRect(150, 170, 30,30 );
+ctx.strokeRect(150, 170, 30,30 );
+ctx.fillRect(90, 170, 30,30 );
+ctx.strokeRect(90, 170, 30,30 );
+ctx.fillRect(60, 170, 30,30 );
+ctx.strokeRect(60, 170, 30,30 );
+ctx.fillRect(210, 170, 30,30 );
+ctx.strokeRect(210, 170, 30,30 );
+ctx.fillRect(270, 170, 30,30 );
+ctx.strokeRect(270, 170, 30,30 );
+ctx.fillRect(300, 170, 30,30 );
+ctx.strokeRect(300, 170, 30,30 );
+
+ctx.fillRect(180, 200, 30,30 );
+ctx.strokeRect(180, 200, 30,30 );
+ctx.fillRect(150, 200, 30,30 );
+ctx.strokeRect(150, 200, 30,30 );
+ctx.fillRect(60, 200, 30,30 );
+ctx.strokeRect(60, 200, 30,30 );
+ctx.fillRect(210, 200, 30,30 );
+ctx.strokeRect(210, 200, 30,30 );
+ctx.fillRect(300, 200, 30,30 );
+ctx.strokeRect(300, 200, 30,30 );
+
+ctx.fillRect(150, 230, 30,30 );
+ctx.strokeRect(150, 230, 30,30 );
+ctx.fillRect(120, 230, 30,30 );
+ctx.strokeRect(120, 230, 30,30 );
+ctx.fillRect(90, 230, 30,30 );
+ctx.strokeRect(90, 230, 30,30 );
+ctx.fillRect(60, 230, 30,30 );
+ctx.strokeRect(60, 230, 30,30 );
+ctx.fillRect(210, 230, 30,30 );
+ctx.strokeRect(210, 230, 30,30 );
+ctx.fillRect(240, 230, 30,30 );
+ctx.strokeRect(240, 230, 30,30 );
+ctx.fillRect(270, 230, 30,30 );
+ctx.strokeRect(270, 230, 30,30 );
+ctx.fillRect(300, 230, 30,30 );
+ctx.strokeRect(300, 230, 30,30 );
+
+ctx.fillRect(300, 260, 30,30 );
+ctx.strokeRect(300, 260, 30,30 );
+ctx.fillRect(270, 260, 30,30 );
+ctx.strokeRect(270, 260, 30,30 );
+ctx.fillRect(210, 260, 30,30 );
+ctx.strokeRect(210, 260, 30,30 );
+ctx.fillRect(180, 260, 30,30 );
+ctx.strokeRect(180, 260, 30,30 );
+ctx.fillRect(150, 260, 30,30 );
+ctx.strokeRect(150, 260, 30,30 );
+ctx.fillRect(90, 260, 30,30 );
+ctx.strokeRect(90, 260, 30,30 );
+ctx.fillRect(60, 260, 30,30 );
+ctx.strokeRect(60, 260, 30,30 );
+
+ctx.fillRect(270, 290, 30,30 );
+ctx.strokeRect(270, 290, 30,30 );
+ctx.fillRect(240, 290, 30,30 );
+ctx.strokeRect(240, 290, 30,30 );
+ctx.fillRect(120, 290, 30,30 );
+ctx.strokeRect(120, 290, 30,30 );
+ctx.fillRect(90, 290, 30,30 );
+ctx.strokeRect(90, 290, 30,30 );
+
+ctx.fillRect(120, 320, 30,30 );
+ctx.strokeRect(120, 320, 30,30 );
+ctx.fillRect(150, 320, 30,30 );
+ctx.strokeRect(150, 320, 30,30 );
+ctx.fillRect(180, 320, 30,30 );
+ctx.strokeRect(180, 320, 30,30 );
+ctx.fillRect(210, 320, 30,30 );
+ctx.strokeRect(210, 320, 30,30 );
+ctx.fillRect(240, 320, 30,30 );
+ctx.strokeRect(240, 320, 30,30 );
+
+ctx.fillStyle = 'Black';
+ctx.fillRect(120, 170, 30,30 );
+ctx.strokeRect(120, 170, 30,30 );
+ctx.fillRect(240, 170, 30,30 );
+ctx.strokeRect(240, 170, 30,30 );
+
+ctx.fillRect(120, 200, 30,30 );
+ctx.strokeRect(120, 200, 30,30 );
+ctx.fillRect(90, 200, 30,30 );
+ctx.strokeRect(90, 200, 30,30 );
+ctx.fillRect(240, 200, 30,30 );
+ctx.strokeRect(240, 200, 30,30 );
+ctx.fillRect(270, 200, 30,30 );
+ctx.strokeRect(270, 200, 30,30 );
+
+ctx.fillRect(180, 230, 30,30 );
+ctx.strokeRect(180, 230, 30,30 );
+
+ctx.fillRect(120, 260, 30,30 );
+ctx.strokeRect(120, 260, 30,30 );
+ctx.fillRect(240, 260, 30,30 );
+ctx.strokeRect(240, 260, 30,30 );
+
+ctx.fillRect(210, 290, 30,30 );
+ctx.strokeRect(210, 290, 30,30 );
+ctx.fillRect(180, 290, 30,30 );
+ctx.strokeRect(180, 290, 30,30 );
+ctx.fillRect(150, 290, 30,30 );
+ctx.strokeRect(150, 290, 30,30 );
